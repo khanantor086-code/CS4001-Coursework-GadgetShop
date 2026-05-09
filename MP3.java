b19729df253da983dce5e03e308a8584ff4c7601
khanantor086-code <khanantor086@gmail.com>
2026-05-09 14:11:15 +0300

    Add MP3 class with memory management features

---
+++ MP3.java
@@ -0,0 +1,30 @@
+public class MP3 extends Gadget {
+    private int availableMemory;
+
+    public MP3(String model, double price, int weight, String size, int availableMemory) {
+        super(model, price, weight, size);
+        this.availableMemory = availableMemory;
+    }
+
+    public int getAvailableMemory() { return availableMemory; }
+
+    public void downloadMusic(int memoryRequired) {
+        if (memoryRequired <= availableMemory) {
+            availableMemory -= memoryRequired;
+            System.out.println("Download successful. Remaining memory: " + availableMemory + " MB.");
+        } else {
+            System.out.println("Insufficient memory to download this music.");
+        }
+    }
+
+    public void deleteMusic(int memoryFreed) {
+        availableMemory += memoryFreed;
+        System.out.println("Music deleted. Available memory: " + availableMemory + " MB.");
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("Available Memory: " + availableMemory + " MB");
+    }
+}
