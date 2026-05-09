97e7341ddc57ed44c576940d138fdbf63c8b0932
khanantor086-code <khanantor086@gmail.com>
2026-05-09 14:10:06 +0300

    Implement Mobile class extending Gadget

---
+++ Mobile.java
@@ -0,0 +1,33 @@
+public class Mobile extends Gadget {
+    private int callingCredit;
+
+    public Mobile(String model, double price, int weight, String size, int callingCredit) {
+        super(model, price, weight, size);
+        this.callingCredit = callingCredit;
+    }
+
+    public int getCallingCredit() { return callingCredit; }
+
+    public void addCredit(int amount) {
+        if (amount > 0) {
+            callingCredit += amount;
+        } else {
+            System.out.println("Please enter a positive amount of credit.");
+        }
+    }
+
+    public void makeCall(String phoneNumber, int duration) {
+        if (callingCredit >= duration) {
+            System.out.println("Calling " + phoneNumber + " for " + duration + " minutes.");
+            callingCredit -= duration;
+        } else {
+            System.out.println("Insufficient credit to make this call.");
+        }
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("Calling Credit: " + callingCredit + " minutes");
+    }
+}
